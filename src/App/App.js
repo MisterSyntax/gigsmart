8922b39cfb464f4d5fd9a84a57e993d1718c9384
Andrew Syriala <asyriala@overstock.com>
2019-04-28 19:16:46 -0600

    Moved App to own folder

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+    return (
+        <div className={styles['app-container']}>
+            Sup world!
+        </div>
+    );
+}
+
+export default App;
