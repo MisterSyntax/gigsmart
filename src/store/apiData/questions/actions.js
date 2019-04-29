3e8909a40bed0de9e93336b1e15972a115de7a75
Andrew Syriala <asyriala@overstock.com>
2019-04-29 00:08:50 -0600

    Add in base event consts, and boilerplate

---
+++ src/store/apiData/questions/actions.js
@@ -0,0 +1,7 @@
+export const QUESTIONS_LOAD = 'QUESTIONS_LOAD';
+export const QUESTIONS_LOAD_FAILURE = 'QUESTIONS_LOAD_FAILURE';
+export const QUESTIONS_LOAD_SUCCESS = 'QUESTIONS_LOAD_SUCCESS';
+
+// export const myEvent = () => ({
+//     type: QUESTIONS_LOAD_SUCCESS,
+// });
