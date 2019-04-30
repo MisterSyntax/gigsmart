e6fd4c944b32b1fa9fc0a1d6ad61905b7c73097d
Andrew Syriala <asyriala@overstock.com>
2019-04-29 23:41:26 -0600

    Add fetch actions

--- src/store/apiData/questions/actions.test.js
+++ src/store/apiData/questions/actions.test.js
@@ -1,12 +1,46 @@
 import {
-    myEvent,
-    MY_EVENT_NAME,
+    failToReceiveQuestions,
+    fetchQuestions,
+    requestQuestions,
+    receiveQuestions,
+    QUESTIONS_LOAD,
+    QUESTIONS_LOAD_FAILURE,
+    QUESTIONS_LOAD_SUCCESS,
 } from './actions';
 
-describe('myEvent', () => {
-    todo('does a thing', () => {
-        expect(myEvent()).toEqual({
-            type: MY_EVENT_NAME,
+describe('requestQuestions', () => {
+    it('dispatches QUESTIONS_LOAD', () => {
+        expect(requestQuestions()).toEqual({
+            type: QUESTIONS_LOAD,
         });
     });
+});
+
+describe('receiveQuestions', () => {
+    it('dispatches QUESTIONS_LOAD', () => {
+        expect(receiveQuestions({ response: 'I am a data' })).toEqual({
+            type: QUESTIONS_LOAD_SUCCESS,
+            data: 'I am a data',
+        });
+    });
+});
+
+describe('requestQuestions', () => {
+    it('dispatches QUESTIONS_LOAD_FAILURE', () => {
+        expect(failToReceiveQuestions('too sad to question')).toEqual({
+            type: QUESTIONS_LOAD_FAILURE,
+            error: 'too sad to question',
+        });
+    });
+});
+
+describe('fetchQuestions', () => {
+    global.fetch = jest.fn(x => x);
+    global.dispatch = jest.fn(x => x);
+    it('fetchQuestions', () => {
+        expect(fetchQuestions('hi')(global.dispatch));
+        expect(global.dispatch.mock.calls.length).toEqual(1);
+        // really should mockup the endpoint too
+    });
+    test.todo('should mock up the endpoint and verify the correct actions are getting dispatched with data');
 });
