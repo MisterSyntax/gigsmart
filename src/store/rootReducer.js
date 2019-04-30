3ac0db47250ce574d65262976a58a633fa8ea4e0
Andrew Syriala <asyriala@overstock.com>
2019-04-29 23:41:47 -0600

    ADd in viewStates

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import config from './config/config';
+import viewStates from './viewStates/viewStates';
 
 export default combineReducers({
     config,
+    viewStates,
 });
