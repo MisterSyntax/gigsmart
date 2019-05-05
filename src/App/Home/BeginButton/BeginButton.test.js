ecd676cad08e60d4a2a54239190e72babfa21245
Andrew Syriala <asyriala@overstock.com>
2019-05-05 01:08:48 -0600

    Update to use fetchQuestions prop

--- src/App/Home/BeginButton/BeginButton.test.js
+++ src/App/Home/BeginButton/BeginButton.test.js
@@ -4,10 +4,10 @@ import { BeginButton } from './BeginButton';
 
 describe('BeginButton', () => {
     it('it should render the begin button and handle clicks to call the beginQuiz button', () => {
-        const beginQuiz = jest.fn(x => x);
-        const wrapper = shallow(<BeginButton beginQuiz={beginQuiz} />);
+        const fetchQuestions = jest.fn(x => x);
+        const wrapper = shallow(<BeginButton fetchQuestions={fetchQuestions} />);
         expect(wrapper.find('.begin-button').length).toEqual(1);
         wrapper.find('.begin-button').simulate('click');
-        expect(beginQuiz.mock.calls.length).toEqual(1);
+        expect(fetchQuestions.mock.calls.length).toEqual(1);
     });
 });
