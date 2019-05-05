8ec3f01635fd81dd1a878ad6afc0cfe38d7c3169
Andrew Syriala <asyriala@overstock.com>
2019-05-05 01:01:01 -0600

    add in apiData

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import apiData from './apiData/apiData';
 import config from './config/config';
 import viewStates from './viewStates/viewStates';
 
 export default combineReducers({
+    apiData,
     config,
     viewStates,
 });
