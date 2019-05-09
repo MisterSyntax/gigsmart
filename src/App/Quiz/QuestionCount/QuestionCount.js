43fe1faeb9275dbf79af275cb7b35f9d208fd2e4
Andrew Syriala <asyriala@overstock.com>
2019-05-08 23:18:33 -0600

    Remove $ type

--- src/App/Quiz/QuestionCount/QuestionCount.js
+++ src/App/Quiz/QuestionCount/QuestionCount.js
@@ -17,7 +17,7 @@ export const QuestionCount = ({
     totalQuestions,
 }) => (
     <div className={styles['question-count']}>
-        {currentQuestionIndex} of ${totalQuestions}
+        {currentQuestionIndex} of {totalQuestions}
     </div>
 );
 
