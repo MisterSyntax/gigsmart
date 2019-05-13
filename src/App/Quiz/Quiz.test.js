587866b6b048b5f32e2658dac48b683f66078975
Andrew Syriala <asyriala@pc-lm31980.domain>
2019-05-12 18:04:53 -0600

    Do not display if completed quiz

--- src/App/Quiz/Quiz.test.js
+++ src/App/Quiz/Quiz.test.js
@@ -4,12 +4,16 @@ import { Quiz } from './Quiz';
 
 describe('Quiz', () => {
     it('should render null when  is not supposed to render', () => {
-        const wrapper = shallow(<Quiz shouldShowQuiz={false} />);
+        const wrapper = shallow(<Quiz hasStartedQuiz={false} />);
+        expect(wrapper.type()).toEqual(null);
+    });
+    it('should render null when  is not supposed to render', () => {
+        const wrapper = shallow(<Quiz hasCompletedQuizs />);
         expect(wrapper.type()).toEqual(null);
     });
 
     it('when it successfully renders it renders an element with the appropriate class-name', () => {
-        const wrapper = shallow(<Quiz shouldShowQuiz />);
+        const wrapper = shallow(<Quiz hasStartedQuiz />);
         expect(wrapper.find('.quiz').length).toEqual(1);
     });
 });
