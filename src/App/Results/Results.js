dcccdc0700583e370af84a407f0c38edfca2a4af
Andrew Syriala <asyriala@pc-lm31980.domain>
2019-05-12 18:41:49 -0600

    Add in base results functionality

---
+++ src/App/Results/Results.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+// --- styles --- //
+import styles from './Results.module.css';
+
+// -- components -- //
+import Score from './Score/Score';
+
+// -- actions -- //
+
+// -- selectors -- //
+import { getHasCompletedQuiz } from '../Quiz/viewStates';
+
+export const Results = ({
+    hasCompletedQuiz,
+}) => {
+    if (!hasCompletedQuiz) {
+        return null;
+    }
+    return (
+        <section className={styles['results-container']}>
+            <Score />
+        </section>
+    );
+};
+
+Results.propTypes = {
+    hasCompletedQuiz: PropTypes.bool,
+};
+
+Results.defaultProps = {
+
+};
+
+/* istanbul ignore next */
+const mapStateToProps = state => ({
+    hasCompletedQuiz: getHasCompletedQuiz(state),
+});
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Results);
