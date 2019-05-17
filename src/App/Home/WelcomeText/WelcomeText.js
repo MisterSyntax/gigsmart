aca35bc2725c82ccaddb45750feadf54cbfe2173
Andrew Syriala <asyriala@pc-lm31980.domain>
2019-05-16 23:06:49 -0600

    clean up text for better display

--- src/App/Home/WelcomeText/WelcomeText.js
+++ src/App/Home/WelcomeText/WelcomeText.js
@@ -1,21 +1,26 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 // --- styles --- //
 import styles from './WelcomeText.module.css';
 
 // -- components -- //
 
+// -- actions -- //
+
+// -- selectors -- //
+import { getQuestionsApiSettingsAmount } from '../../../store/config/selectors/questionApi/config';
+
 export const WelcomeText = ({
     numberOfQuestions,
 }) => (
     <Fragment>
         <h1 className={styles['welcome-header']}>Welcome To The Trivia Dome</h1>
+
         <div className={styles['welcome-text']}>
-            You will be presented with {numberOfQuestions} questions
-        </div>
-        <div className={styles['welcome-text']}>
-            Can you score 100%?
+            <p>You will be presented with {numberOfQuestions} questions</p>
+            <p>Can you score 100%?</p>
         </div>
     </Fragment>
 );
@@ -28,4 +33,12 @@ WelcomeText.defaultProps = {
 
 };
 
-export default WelcomeText;
+const mapStateToProps = state => ({
+    numberOfQuestions: getQuestionsApiSettingsAmount(state),
+});
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(WelcomeText);
